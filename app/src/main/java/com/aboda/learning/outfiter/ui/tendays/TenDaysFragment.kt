65d4473de177320ac4443f459ea0ced50fd6f9dd
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-09 20:30:20 +0200

    add strings create ten day, outfit and today fragments

---
+++ app/src/main/java/com/aboda/learning/outfiter/ui/tendays/TenDaysFragment.kt
@@ -0,0 +1,20 @@
+package com.aboda.learning.outfiter.ui.tendays
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aboda.learning.outfiter.R
+
+class TenDaysFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ten_days, container, false)
+    }
+
+}
