b6478be29faba91bfe205bd2f37bdccc912878ef
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-09 05:07:54 +0200

    initial commit

---
+++ app/src/main/java/com/aboda/learning/outfiter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aboda.learning.outfiter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
