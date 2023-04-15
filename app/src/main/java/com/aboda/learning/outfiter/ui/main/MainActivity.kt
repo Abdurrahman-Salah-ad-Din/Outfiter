493473887b74daa7e1f6c4211bdabdf371ccbc58
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-15 22:03:59 +0200

    rename getList method create addCallback method rename fetchData method refactor getAllDailyWeather method reformat file

--- app/src/main/java/com/aboda/learning/outfiter/ui/main/MainActivity.kt
+++ app/src/main/java/com/aboda/learning/outfiter/ui/main/MainActivity.kt
@@ -17,23 +17,30 @@ import com.google.android.material.tabs.TabLayoutMediator
 class MainActivity : AppCompatActivity(), NetworkService {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        fetchData(EgyptianCity.CAIRO)
-        binding.searchView.editText.addTextChangedListener (object : TextWatcher {
+        addCallback()
+        fetchWeatherDataForCity(EgyptianCity.CAIRO)
+    }
+
+    private fun addCallback() {
+        binding.searchView.editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // Do nothing
             }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val searchResult = getList(s.toString())
+            override fun onTextChanged(
+                newText: CharSequence?, start: Int, before: Int, count: Int
+            ) {
+                val searchResult = getSearchResult(newText.toString())
                 val searchCityAdapter = SearchCityAdapter(object : OnCityClickListener {
                     override fun onCityClick(city: EgyptianCity) {
                         binding.searchBar.hint = city.cityName
                         binding.searchView.hide()
-                        fetchData(city)
+                        fetchWeatherDataForCity(city)
                     }
                 })
                 searchCityAdapter.submitList(searchResult)
@@ -46,31 +53,39 @@ class MainActivity : AppCompatActivity(), NetworkService {
         })
     }
 
-    private fun getList(text: String): List<EgyptianCity> {
+    private fun getSearchResult(text: String): List<EgyptianCity> {
         return EgyptianCity.values().filter { it.cityName.contains(text) }
     }
 
-    private fun fetchData(city: EgyptianCity) {
+    private fun fetchWeatherDataForCity(city: EgyptianCity) {
         val api = ApiService(this, city)
         api.fetchData()
     }
 
     override fun getAllDailyWeather(allDailyWeather: List<DailyWeather>) {
         runOnUiThread {
+            val weatherToday = allDailyWeather[0]
+
+            val todayFragment = TodayFragment()
+            todayFragment.createBundleWithParcelable(weatherToday)
+
+            val outfitFragment = OutfitFragment()
+            outfitFragment.createBundleWithParcelable(weatherToday)
+
+            val tenDaysFragment = TenDaysFragment()
+            tenDaysFragment.createBundleWithParcelableArray(allDailyWeather.toTypedArray())
+
             val fragmentsAdapter = FragmentsAdapter(
-                this,
-                listOf(
-                    TodayFragment(allDailyWeather[0]),
-                    OutfitFragment(allDailyWeather[0]),
-                    TenDaysFragment(allDailyWeather)
+                this, listOf(
+                    todayFragment, outfitFragment, tenDaysFragment
                 )
             )
             binding.fragmentContainer.adapter = fragmentsAdapter
             TabLayoutMediator(binding.tabLayout, binding.fragmentContainer) { tab, position ->
                 when (position) {
-                    0 -> tab.text = "Today"
-                    1 -> tab.text = "Outfit"
-                    2 -> tab.text = "10 Days"
+                    0 -> tab.text = todayFragment.name
+                    1 -> tab.text = outfitFragment.name
+                    2 -> tab.text = tenDaysFragment.name
                 }
             }.attach()
         }
