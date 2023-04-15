833e1fd7264782be58bd8ea932cde4cfd723294a
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-15 22:19:23 +0200

    finish Ten day fragment use base fragment remove onCreateView method override onViewCreated method

--- app/src/main/java/com/aboda/learning/outfiter/ui/tendays/TenDaysFragment.kt
+++ app/src/main/java/com/aboda/learning/outfiter/ui/tendays/TenDaysFragment.kt
@@ -1,24 +1,25 @@
 package com.aboda.learning.outfiter.ui.tendays
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.aboda.learning.outfiter.data.model.DailyWeather
 import com.aboda.learning.outfiter.databinding.FragmentTenDaysBinding
+import com.aboda.learning.outfiter.ui.main.BaseFragment
+import com.aboda.learning.outfiter.ui.utils.ALL_DAILY_WEATHER
 
-class TenDaysFragment(private val allDailyWeather: List<DailyWeather>) : Fragment() {
 
-    private lateinit var binding: FragmentTenDaysBinding
+class TenDaysFragment() : BaseFragment<FragmentTenDaysBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentTenDaysBinding.inflate(layoutInflater, container, false)
+    override val bindingInflater: (LayoutInflater) -> FragmentTenDaysBinding =
+        FragmentTenDaysBinding::inflate
+
+    private lateinit var allDailyWeather: List<DailyWeather>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        allDailyWeather = arguments?.getParcelableArray(ALL_DAILY_WEATHER)!!.toList() as List<DailyWeather>
         loadDataToRecyclerView()
-        return binding.root
     }
 
     private fun loadDataToRecyclerView() {
